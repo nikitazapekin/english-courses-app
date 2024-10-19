313edde4a8ac4a3f16e7bd5790662a47dbad29aa
nikitazapekin <wotblitz362@mail.ru>
2024-10-19 20:03:50 +0300

    add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,46 @@
 import styles from "./Header.module.scss"
+import Logo from "../../assets/logo.png"
+import Search from "../../assets/search.png"
 const Header = () => {
-    return ( 
-      <header className="header">
+    return (
+        <header className={styles.header}>
+            <div className={styles.header__inner}>
+                <div className={styles.header__preview}>
+                    <img src={Logo} alt="Logo" />
+                    <div className={styles.search}>
+                        <input type="text"
+                            placeholder="Найти..."
+                            className={styles.search__input} />
+                        <div className={styles.search__btn}>
+                            <img src={Search} alt="Icon"
+                                className={styles.search__icon}
+                            />
+                        </div>
+                    </div>
+                </div>
+                    <nav className={styles.navigation}>
+                        <ul className={styles.navigation__list}>
+                            <li className={styles.navigation__item}>
+                                <div className={styles.navigation__text}>
+                                    Каталог
+                                </div>
+                            </li>
+                            <li className={styles.navigation__item}>
+                                <div className={`${styles.navigation__btn} ${styles.navigation__text}`}>
+                                    Записаться на урок
+                                </div>
+                            </li>
+                            <li className={styles.navigation__item}>
+                                <div className={styles.navigation__text}>
+                                    Войти
+                                </div>
+                            </li>
 
-      </header>
-     );
+                        </ul>
+                    </nav>
+            </div>
+        </header>
+    );
 }
- 
+
 export default Header;
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header/Header"
+import styles from "../theme/wrappers.module.scss"
+import "../theme/normalize.scss"
+const Homepage = () => {
+    return (
+        <div className={styles.wrapper}>
+            <Header />
+            <div className={styles.content}>
+                sqf
+            </div>
+            qwf
+        </div>
+    );
+}
+
+export default Homepage;
