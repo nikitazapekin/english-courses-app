e67c683a6d801d5a7a41eda86b9bd30770b70b31
nikitazapekin <wotblitz362@mail.ru>
2024-11-26 11:46:48 +0300

    timeout

--- src/components/CategoriesComponent/CategotiesComponent.tsx
+++ src/components/CategoriesComponent/CategotiesComponent.tsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
 import CoursesList from "../CoursesList/CoursesList";
 import SearchPanel from "../SearchPanel/SearchPanel";
 import SearchToolbar from "../SearchToolbar/SearchToolbar";
 import styles from "./CategoriesComponent.module.scss"
+
 const CategoriesComponent = () => {
+   const [query, searchQuery] = useState("")
+   const handleSearch = () => {
+
+   }
+   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    searchQuery(event.target.value);  
+};
+
+
     return (
 
         <section className={styles.categories}>
             <div className={styles.categories__inner}>
                 <SearchToolbar />
+             
                 <div className={styles.categories__content}>
 
-                    <SearchPanel />
-                    <CoursesList />
+                    <SearchPanel  handleChange={handleChange} />
+                    <CoursesList query={query} />
                 </div>
             </div>
         </section>
--- src/components/CoursesList/CoursesList.tsx
+++ src/components/CoursesList/CoursesList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./CoursesList.module.scss";
 import Card from "../../assets/cards/card1.png";
 import Design from "../../assets/cards/design.png";
@@ -162,10 +162,14 @@ const initialCards: Course[] = [
         image: Design,
         color: "#D1D803"
     },
-   
+
 ];
 
-const CoursesList: React.FC = () => {
+interface CoursesListProps {
+    query: string
+}
+
+const CoursesList = ({ query }: CoursesListProps) => {
     const [cards, setCards] = useState<Course[]>(initialCards);
     const [sortOption, setSortOption] = useState<SortOption>("price");
 
@@ -184,6 +188,21 @@ const CoursesList: React.FC = () => {
         setSortOption(selectedOption);
         sortCards(selectedOption);
     };
+
+    useEffect(() => {
+        if (query) {
+
+            setCards(
+                initialCards.filter((item) =>
+                    item.title.toLowerCase().includes(query.toLowerCase()) ||
+                    item.name.toLowerCase().includes(query.toLowerCase())
+                )
+            );
+        } else {
+            setCards(initialCards);
+        }
+    }, [query]);
+
 
     return (
         <div className={styles.courses}>
@@ -204,19 +223,25 @@ const CoursesList: React.FC = () => {
                 <div className={styles.courses__cards}>
                     {cards.map((item, index) => (
                         <Link to="/card">
-                        <div className={styles.card} key={index}>
-                            <div className={styles.card__preview} style={{ backgroundColor: item.color }}>
-                                <p className={styles.card__name}>{item.name}</p>
-                                <img className={styles.card__image} src={item.image} alt={item.title} />
+                            <div className={styles.card} key={index}>
+                                <div className={styles.card__preview} style={{ backgroundColor: item.color }}>
+                                    <p className={styles.card__name}>{item.name}</p>
+                                    <img className={styles.card__image} src={item.image} alt={item.title} />
+                                </div>
+                                <h3 className={styles.card__title}>{item.title}</h3>
+                                <p className={styles.card__price}>{`${item.price}$`}</p>
+                                <p className={styles.card__rating}>Рейтинг: {item.rating}</p>
+                                <p className={styles.card__releaseDate}>Дата выпуска: {item.releaseDate}</p>
                             </div>
-                            <h3 className={styles.card__title}>{item.title}</h3>
-                            <p className={styles.card__price}>{`${item.price}$`}</p>
-                            <p className={styles.card__rating}>Рейтинг: {item.rating}</p>
-                            <p className={styles.card__releaseDate}>Дата выпуска: {item.releaseDate}</p>
-                        </div>
                         </Link>
                     ))}
+
                 </div>
+                    {cards.length == 0 && (
+                        <div className={styles.courses__text}>
+                            По вашему запросу "{query}" ничего не найдено
+                        </div>
+                    )}
 
                 <CoursesListDots />
             </div>
@@ -225,176 +250,3 @@ const CoursesList: React.FC = () => {
 };
 
 export default CoursesList;
-
-/*
-
-import styles from "./CoursesList.module.scss"
-import Card from "../../assets/cards/card1.png"
-import Design from "../../assets/cards/design.png"
-import CoursesListDots from "./CoursesListDots"
-const cards = [
-    {
-        name: "Web",
-        title: "Web-технологии",
-        price: "30$",
-        image: Card,
-        color: "#0389D8"
-    },
-    {
-        name: "Design",
-        title: "Для дизайнеров",
-        price: "50$",
-        image: Design,
-        color: "#D1D803"
-    },
-    {
-        name: "Web",
-        title: "Web-технологии",
-        price: "30$",
-        image: Card,
-        color: "#0389D8"
-    },
-    {
-        name: "Web",
-        title: "Web-технологии",
-        price: "30$",
-        image: Card,
-        color: "#0389D8"
-    },
-    {
-        name: "Web",
-        title: "Web-технологии",
-        price: "30$",
-        image: Card,
-        color: "#0389D8"
-    },
-    {
-        name: "Web",
-        title: "Web-технологии",
-        price: "30$",
-        image: Card,
-        color: "#0389D8"
-    },
-
-
-
-    {
-        name: "Web",
-        title: "Web-технологии",
-        price: "30$",
-        image: Card,
-        color: "#0389D8"
-    },
-    {
-        name: "Web",
-        title: "Web-технологии",
-        price: "30$",
-        image: Card,
-        color: "#0389D8"
-    },
-    {
-        name: "Web",
-        title: "Web-технологии",
-        price: "30$",
-        image: Card,
-        color: "#0389D8"
-    },
-    {
-        name: "Web",
-        title: "Web-технологии",
-        price: "30$",
-        image: Card,
-        color: "#0389D8"
-    },
-    {
-        name: "Web",
-        title: "Web-технологии",
-        price: "30$",
-        image: Card,
-        color: "#0389D8"
-    },
-    {
-        name: "Web",
-        title: "Web-технологии",
-        price: "30$",
-        image: Card,
-        color: "#0389D8"
-    },
-
-
-
-    {
-        name: "Web",
-        title: "Web-технологии",
-        price: "30$",
-        image: Card,
-        color: "#0389D8"
-    },
-    {
-        name: "Web",
-        title: "Web-технологии",
-        price: "30$",
-        image: Card,
-        color: "#0389D8"
-    },
-    {
-        name: "Web",
-        title: "Web-технологии",
-        price: "30$",
-        image: Card,
-        color: "#0389D8"
-    },
-    {
-        name: "Web",
-        title: "Web-технологии",
-        price: "30$",
-        image: Card,
-        color: "#0389D8"
-    },
-
-]
-const CoursesList = () => {
-    return (
-        <div className={styles.courses}>
-            <div className={styles.courses__inner}>
-                <div className={styles.courses__header}>
-                    <h2 className={styles.courses__title}>
-                        Новинки
-                    </h2>
-                    <select className={styles.courses__select}>
-                        <option className={styles.courses__option}>Сортировать по стоимости</option>
-                        <option className={styles.courses__option}>Сортировать по рейтингу</option>
-                        <option className={styles.courses__option}>Сортировать по выпуску</option>
-                    </select>
-
-                </div>
-
-                <div className={styles.courses__cards}>
-                    {cards.map((item, index) => (
-
-                        <div className={styles.card} key={index}>
-                            <div className={styles.card__preview} style={{ backgroundColor: item.color }}>
-                                <p className={styles.card__name}>
-                                    {item.name}
-                                </p>
-                                <img className={styles.card__image} src={item.image} />
-                            </div>
-
-                            <h3 className={styles.card__title}>
-                                {item.title}
-                            </h3>
-                            <p className={styles.card__price}>
-                                {item.price}
-                            </p>
-                        </div>
-                    ))}
-                </div>
-
-                <CoursesListDots />
-            </div>
-        </div>
-    );
-}
-
-export default CoursesList;
-*/
