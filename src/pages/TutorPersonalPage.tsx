217ba57fee0a4b65400d6e96d367bb569b5548f0
nikitazapekin <wotblitz362@mail.ru>
2025-02-18 23:18:46 +0300

    add get tutor method

--- src/pages/TutorPersonalPage.tsx
+++ src/pages/TutorPersonalPage.tsx
@@ -7,16 +7,33 @@ import { useEffect } from "react";
 import TutorAdd from "../components/Tutor/TutorAdd/TutorAdd";
 import HelpBtn from "../components/HelpBtn/HelpBtn";
 import NavigateBtn from "../components/NavigateBtn/NavigateBtn";
+import { useDispatch } from "react-redux";
+import TutorService from "../services/Tutor";
 const TutorPersonalPage = () => {
 
 
+   
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
     useEffect(() => {
 
-        window.scrollTo(0, 0);
+        window.scrollTo(0, 0)
+        const handleGetUser = async () => {
+            try {
+                const response = await TutorService.GetTutor()
+                console.log("RESP", JSON.stringify(response))
+               // dispatch(setPerson( response.data.user))
+            } catch (err) {
+                navigate("/sign-in")
+                console.log("Something went wrong", err);
+            }
+        };
+        handleGetUser();  
+    
+
 
     }, []);
-
-
 
 
 
---
+++ src/services/Tutor.ts
@@ -0,0 +1,30 @@
+import $api from "../http";
+import axios, { AxiosResponse } from 'axios';
+import { RegisterInterface } from "../components/SignUpForm/types";
+import { SignInData } from "../components/SignInForm/types";
+
+interface GetTutorProps {
+
+    message: string,
+    user: {
+        id:number
+        id_author: null,
+        username: string,
+        email:string,
+       
+        description: string,
+        rate:string,
+        specialization:string,
+        english_level: string,
+        full_description: string,
+        role: string,
+    }
+
+}
+export default class TutorService {
+    static async GetTutor(): Promise<AxiosResponse<GetTutorProps>> {
+        return $api.get<GetTutorProps>('/tutor/getTutor');
+    }
+ 
+
+}
