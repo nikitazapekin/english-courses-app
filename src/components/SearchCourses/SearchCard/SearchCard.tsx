6ba1e7a7fcb8a3707f9f58d538a32685ae68fe04
nikitazapekin <wotblitz362@mail.ru>
2025-02-24 12:33:27 +0300

    add cards

---
+++ src/components/SearchCourses/SearchCard/SearchCard.tsx
@@ -0,0 +1,35 @@
+import styles from "./SearchCard.module.scss"
+
+
+interface Course {
+    item: {
+
+        id: number,
+        author: string,
+        title: string,
+        description: string,
+        course_for: String[],
+        release_date: string,
+        course_logo: string,
+    }
+}
+
+const SearchCard = ({ item }: Course) => {
+    return (
+        <div className={styles.card}>
+            <img
+                className={styles.card__image}
+                alt="logo"
+                src={item.course_logo}
+            />
+            <h3 className={styles.card__title}>
+                {item.title}
+            </h3>
+            <h4 className={styles.card__date}>
+                {item.release_date}
+            </h4>
+
+        </div>);
+}
+
+export default SearchCard;
--- src/components/SearchCourses/SearchCourses.tsx
+++ src/components/SearchCourses/SearchCourses.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import styles from "./SearchCourses.module.scss"
 import CourseService from "../../services/Course";
-
+import { useParams } from "react-router-dom";
+import SearchCard from "./SearchCard/SearchCard";
 
 
 interface Course {
@@ -17,31 +18,47 @@ interface Course {
 
 const SearchCourses = () => {
 
-    // const cards
-
+    const { query } = useParams<{ query: string }>();
     const [cards, setCards] = useState<Course[]>([])
     useEffect(() => {
         const handleSearch = async () => {
             try {
-                const response = await CourseService.SearchCourses("мед")
-                setCards(response.data.courses)
+                if (query) {
+
+                    const response = await CourseService.SearchCourses(query)
+                    setCards(response.data.courses)
+                }
             } catch {
 
             }
         }
         handleSearch()
     }, [])
-    return (<div>
-        {cards.map(item => (
-            <div>
-                {item.title}
+    return (<div className={styles.courses}>
+        {
+            cards.length == 0 && (
 
-                <img
-                alt="logo"
-                src={item.course_logo}
+                <h1 className={styles.courses__title}>
+                    По запросу {query} ничего не найдено
+                </h1>
+            )
+        }
+         {
+            cards.length > 0 && (
+
+                <h1 className={styles.courses__title}>
+                    По запросу {query} найдено:
+                </h1>
+            )
+        }
+        <div className={styles.courses__cards}>
+            {cards.map(item => (
+                <SearchCard
+                    key={item.id}
+                    item={item}
                 />
-            </div>
-        ))}
+            ))}
+        </div>
     </div>);
 }
 
