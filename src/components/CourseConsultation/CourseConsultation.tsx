2c2c972e20d01b19e58ba8a087c4967ef28e873c
nikitazapekin <wotblitz362@mail.ru>
2025-04-13 20:15:53 +0300

    send

--- src/components/CourseConsultation/CourseConsultation.tsx
+++ src/components/CourseConsultation/CourseConsultation.tsx
@@ -1,5 +1,36 @@
+import { useState } from "react"
+import MailService from "../../services/Mail"
 import styles from "./CourseConsultation.module.scss"
+import { useSelector } from "react-redux"
+import { OpenCourseSelector } from "../../store/selectors/OpenCourseSelector"
 const CourseConsultation = () => {
+    const [data, setData] = useState({
+        email: "",
+        firstName: "",
+        secondName: "",
+        message: ""
+    })
+
+  /*   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+    } */
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setData(prev => ({
+        ...prev,
+        [name]: value
+    }))
+}
+
+    const course = useSelector(OpenCourseSelector)
+    const handleSend = async () => {
+        try {
+            const resp = await MailService.SendMessage(data.email, data.firstName, data.secondName, data.message, course.tutor.email)
+        } catch (e) {
+            console.log(e)
+        }
+    }
     return (<section className={styles.course}>
         <form className={styles.course__inner}>
             <div className={styles.course__content}>
@@ -7,9 +38,10 @@ const CourseConsultation = () => {
                     Записаться на пробное занятие с репетитором
                 </h2>
                 <div className={styles.form}>
-                    <input className={styles.form__input} type="text" placeholder="Имя" required />
-                    <input className={styles.form__input} type="text" placeholder="Фамилия" required />
-                    <input className={styles.form__input} type="text" placeholder="Почта" required />
+                    <input className={styles.form__input} onChange={(e) => handleChange(e)} name="email" type="text" placeholder="Имя" required />
+                    <input className={styles.form__input} onChange={(e) => handleChange(e)} name="firstName" type="text" placeholder="Фамилия" required />
+                    <input className={styles.form__input} onChange={(e) => handleChange(e)} name="secondName" type="text" placeholder="Почта" required />
+                    <input className={styles.form__input} onChange={(e) => handleChange(e)} name="message" type="text" placeholder="Сообщение" required />
                 </div>
             </div>
             <div className={styles.course__personal}>
@@ -18,9 +50,9 @@ const CourseConsultation = () => {
                     Я согласен на обработку персональных данных
                 </p>
             </div>
-            <button className={styles.course__btn} type="submit">
+            <button className={styles.course__btn} type="button" onClick={handleSend}>
                 <p className={styles.course__btn__text}>
-                    Записаться
+                    Отправить
                 </p>
                 <div className={styles.course__btn__background} />
             </button>
@@ -30,3 +62,68 @@ const CourseConsultation = () => {
 }
 
 export default CourseConsultation;
+
+/*
+import styles from "./AboutAuthor.module.scss"
+import Agree from "../../assets/courseDetails/agree.png"
+import Irina from "../../assets/courseDetails/irina.jpg"
+import { useNavigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { OpenCourseSelector } from "../../store/selectors/OpenCourseSelector"
+const AboutAuthor = () => {
+  
+    const navigate = useNavigate()
+    const handleNavigate = () => {
+        navigate(`/tutor/${course.tutor.id}`)
+    }
+    const course = useSelector(OpenCourseSelector)
+    return (
+        <section className={styles.about}>
+            <div className={styles.about__inner}>
+                <h2 className={styles.about__title}>
+                    О авторе курса
+                </h2>
+                <div className={styles.about__content}>
+                    <div className={styles.about__author__wrapper}>
+
+                        <img src={course.tutor.avatar_base64 ? course.tutor.avatar_base64 : ""} alt="Author" className={styles.about__author} />
+                    </div>
+                    <div className={styles.info}>
+                        <h3 className={styles.info__title}>
+                            {course.tutor.username}
+                        </h3>
+                        <p className={styles.info__citate}>
+                            {course.tutor.description}
+                        </p>
+                        <div className={styles.info__skills}>
+                            {
+                                course.tutor.experience.map((item, index) => (
+                                    <div className={styles.info__skill} key={index}>
+                                        <img src={Agree} alt="Agree" />
+                                        <p className={styles.info__text}>
+                                            {item}
+                                        </p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                        <div className={styles.info__btn}>
+                            <p className={styles.info__btn__text} onClick={handleNavigate} >
+
+                                Подробнее
+                            </p>
+                            <div className={styles.info__btn__background} />
+
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </section>);
+}
+
+export default AboutAuthor;
+*/
---
+++ src/services/Mail.ts
@@ -0,0 +1,44 @@
+
+import $api from "../http";
+import { AxiosResponse } from 'axios';
+
+export default class MailService {
+    static async SendMessage(
+        email: string,
+        firstName: string,
+        secondName: string,
+        message: string,
+        author: string
+    ): Promise<AxiosResponse> {
+        return $api.post('/mail/sendMessage', { 
+            email, 
+            firstName, 
+            secondName, 
+            message, 
+            author 
+        }, {
+            headers: {
+                'Content-Type': 'application/json' // Изменено на правильный тип
+            }
+        });
+    }
+}
+/* import $api from "../http";
+import axios, { AxiosResponse } from 'axios';
+
+
+export default class MailService {
+    static async SendMessage(email: string,
+        firstName: string,
+        secondName: string,
+        message: string,
+        author: string,): Promise<AxiosResponse<any>> {
+        return $api.post<any>('/mail/sendMessage', { email, firstName, secondName, message, author }, {
+            headers: {
+                'Content-Type': 'application/json' 
+            }
+        });
+    }
+
+
+} */
