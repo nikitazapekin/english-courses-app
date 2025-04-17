ebd4be4ac0599cb735d1832a6296c46b7c4853b5
nikitazapekin <wotblitz362@mail.ru>
2025-04-17 17:36:22 +0300

    last

--- src/components/Tutor/TutorCourse/TutorCourse.tsx
+++ src/components/Tutor/TutorCourse/TutorCourse.tsx
@@ -4,6 +4,142 @@ import { useState } from "react";
 import WarningModal from "../../CategoriesComponent/WarningModal/WarningModal";
 import WarningsModal from "./WarningsModal/WarningsModal";
 
+interface Warning {
+    id: number,
+    warning_text: string,
+    warning_date: string,
+    is_active: boolean
+}
+
+interface Ban {
+    id: number,
+    ban_text: string,
+    ban_date: string,
+    is_active: boolean
+}
+
+interface TutorCourseProps {
+    item: {
+        id: number;
+        author: string;
+        title: string;
+        description: string;
+        course_for: String[];
+        release_date: string;
+        course_logo: string;
+        bans: number[],
+        isvisible: boolean,
+        warnings: number[],
+        warnings_data: Warning[],
+        bans_data: Ban[],
+    };
+}
+
+const TutorCourse = ({ item }: TutorCourseProps) => {
+    const navigate = useNavigate();
+    const [isOpenBansInfo, setIsOpenBansInfo] = useState(false);
+    const [isOpenWarningsInfo, setIsOpenWarningsInfo] = useState(false);
+
+    const handleRedirect = (event: React.MouseEvent) => {
+        event.stopPropagation();
+        navigate(`/tutor/personal/courses/${item.id}`);
+    };
+
+    const handleRedirectCourse = (event: React.MouseEvent) => {
+        if (!isOpenWarningsInfo && !isOpenBansInfo) {
+            navigate(`/card/lessons/${item.id}`);
+        }
+    };
+
+    const handleOpenBansInfo = (event: React.MouseEvent) => {
+        event.stopPropagation();
+        setIsOpenBansInfo(prev => !prev);
+    };
+
+    const handleOpenWarningsInfo = (event: React.MouseEvent) => {
+        event.stopPropagation();
+        setIsOpenWarningsInfo(prev => !prev);
+    };
+
+    return (
+        <div
+            className={styles.card}
+            onClick={handleRedirectCourse}
+        >
+            <div className={styles.card__content}>
+                <img
+                    className={styles.card__image}
+                    src={item.course_logo}
+                    alt="logo"
+                />
+                <div className={styles.card__preview}>
+                    <h3 className={styles.card__title}>
+                        {item.title}
+                    </h3>
+                    <p className={styles.card__describtion}>
+                        {item.description}
+                    </p>
+
+                    <div className={styles.card__for}>
+                        {item.course_for.map((item_for, index) => (
+                            <div className={styles.card__for__item} key={index}>
+                                {item_for}
+                            </div>
+                        ))}
+                    </div>
+
+                    <div>
+                        <p className={styles.vis}>
+                            Курс виден другим пользователям: {item.isvisible ? "Да" : "Нет"}
+                        </p>
+                        {item.bans != null && item.bans.length > 0 && (
+                            <div className={styles.bans}>
+                                Ваш курс заблокирован. <span 
+                                    className={`${styles.bans} ${styles.underline}`}
+                                    onClick={handleOpenBansInfo}
+                                >
+                                    Узнать причину
+                                </span>
+                            </div>
+                        )}
+                        {item.warnings_data != null && item.warnings_data.length > 0 && (
+                            <div className={styles.warnings}>
+                                У вашего курса есть предупреждения. <span 
+                                    className={`${styles.warnings} ${styles.underline}`}
+                                    onClick={handleOpenWarningsInfo}
+                                >
+                                    Подробнее
+                                </span>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+
+            <p
+                className={styles.card__edit}
+                onClick={handleRedirect}
+            >
+                Редактировать
+            </p>
+
+            {isOpenWarningsInfo && (
+                <WarningsModal 
+                    warnings={item.warnings_data} 
+                    handler={handleOpenWarningsInfo}
+                />
+            )}
+        </div>
+    );
+};
+
+export default TutorCourse;
+/* import { useNavigate } from "react-router-dom";
+import styles from "./TutorCourse.module.scss";
+import { useState } from "react";
+import WarningModal from "../../CategoriesComponent/WarningModal/WarningModal";
+import WarningsModal from "./WarningsModal/WarningsModal";
+
 
 interface Warning {
     id: number,
@@ -133,3 +269,4 @@ const TutorCourse = ({ item }: TutorCourseProps) => {
 };
 
 export default TutorCourse;
+ */
--- src/components/Tutor/TutorCourse/WarningsModal/WarningsModal.tsx
+++ src/components/Tutor/TutorCourse/WarningsModal/WarningsModal.tsx
@@ -1,4 +1,53 @@
-import { useDispatch } from "react-redux";
+import styles from "./WarningsModal.module.scss";
+
+interface Warning {
+    id: number,
+    warning_text: string,
+    warning_date: string,
+    is_active: boolean
+}
+
+interface Props {
+    warnings: Warning[],
+    handler: (e: React.MouseEvent) => void
+}
+
+const WarningsModal = ({ warnings, handler }: Props) => {
+    const handleClose = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        handler(e);
+    };
+
+    const handleContentClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
+    };
+
+    return (
+        <div className={styles.modal} onClick={handleClose}>
+            <div className={styles.modal__content} onClick={handleContentClick}>
+                <h1 className={styles.modal__title}>
+                    Предупреждения курса:
+                </h1>
+                {warnings.map(item => (
+                    <div key={item.id} className={styles.card}>
+                        <p className={styles.card__text}>
+                            {item.warning_text}
+                        </p>
+                        <p className={styles.card__date}>
+                            {item.warning_date}
+                        </p>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.modal__overlay} />
+
+           
+        </div>
+    );
+};
+
+export default WarningsModal;
+/* import { useDispatch } from "react-redux";
 
 import styles from "./WarningsModal.module.scss"
 import { addAchievement, closeAchievement, setOpenModalAchievements, updateAchievement, } from "../../../../store/slices/AddAchievementSlice/AddAchievementSlice";
@@ -68,3 +117,4 @@ export default WarningsModal;
 
 
 
+ */
